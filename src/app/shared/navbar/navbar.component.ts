f0969911d89e8c583a6191d422de8d1fb79e6271
Riturajsharma707 <riturajsharma54321@gmail.com>
2024-08-13 19:35:49 +0530

    menuItem updated

--- src/app/shared/navbar/navbar.component.ts
+++ src/app/shared/navbar/navbar.component.ts
@@ -1,11 +1,17 @@
+import { NgIf } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, NgIf],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css',
 })
-export class NavbarComponent {}
+export class NavbarComponent {
+  visible: boolean = false;
+  menuItem() {
+    this.visible = !this.visible;
+  }
+}
